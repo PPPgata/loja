a26daeca7aaf064bf9d755fbc809e2f41cccd78e
PPPgata <pedrodaniel8416@gmail.com>
2023-09-29 14:59:31 -0300

    teste2

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let items = [];
 
+function atualizarLista() {
+    let lista_produtos = document.querySelector('.lista_produtos');
+    let soma = 0;
+    lista_produtos.innerHTML = '';
 
-document.querySelector('.lista_cadastro input[type=submit]')
-.addEventListener('click',()=>{
+    items.forEach(function (val, indice) {
+        soma += parseFloat(val.preco);
+        lista_produtos.innerHTML += `
+            <div class="lista_produtos_single">
+                <h3>${val.nome}</h3>
+                <input type="button" value="excluir" class="apagar_item" data-indice=${indice}>
+                <h3 class="price_produto"><span>R$${parseFloat(val.preco).toFixed(2)}</span></h3>
+            </div>
+        `;
+    });
+
+    soma = soma.toFixed(2);
+    let elemento_soma = document.querySelector('.soma h1');
+    elemento_soma.innerHTML = 'Total: R$' + soma;
+
+    adicionarEventosExcluir();
+}
+
+
+function adicionarItem() {
     let nome_produto = document.querySelector('.lista_cadastro input[name=nome_produto]');
     let valor_produto = document.querySelector('.lista_cadastro input[name=valor_produto]');
 
@@ -10,57 +32,32 @@ document.querySelector('.lista_cadastro input[type=submit]')
         nome: nome_produto.value,
         preco: valor_produto.value
     });
-  
-    let lista_produtos = document.querySelector('.lista_produtos');
-    let soma = 0;
-    lista_produtos.innerHTML = ''
-    items.map(function(val){
-        soma +=parseFloat(val.preco)
-        lista_produtos.innerHTML += `
-        
-        <div class="lista_produtos_single">
-                <h3>`+val.nome+`</h3>
-                <input type="submit" value="excluir" name="apagar_item">
-                <h3 class="price_produto"><span>R$` + parseFloat(val.preco).toFixed(2) + `</span></h3>
-            </div>
-        
-        `
-    })
-    
-    soma = soma.toFixed(2)
+
     nome_produto.value = '';
     valor_produto.value = '';
 
-    let elemento_soma = document.querySelector('.soma h1')
-    elemento_soma.innerHTML = 'Total: R$'+soma
-})
-
-let botao_apagar = document.querySelectorAll('.apagar_item')
-botao_apagar.forEach((botao,index)=>{
-        botao.addEventListener('click',()=>{
-            items.splice(index,1)
-
-            lista_produtos.innerHTML = '';
-            soma = 0;
-            items.map(function (val, index) {
-                soma += parseFloat(val.preco);
-                lista_produtos.innerHTML += `
-                    <div class="lista_produtos_single">
-                        <h3>` + val.nome + `</h3>
-                        <input type="submit" value="limpar" class="apagar_item">
-                        <h3 class="price_produto"><span>R$` + parseFloat(val.preco).toFixed(2) + `</span></h3>
-                    </div>
-                `
-            })
-        })
-})
+    atualizarLista();
+}
 
 
-document.querySelector('button[name=limpar]')
-.addEventListener('click',()=>{
+function adicionarEventosExcluir() {
+    let botao_apagar = document.querySelectorAll('.apagar_item');
+    botao_apagar.forEach((botao) => {
+        botao.addEventListener('click', () => {
+            let indice = botao.getAttribute('data-indice');
+            items.splice(indice, 1);
+            atualizarLista();
+        });
+    });
+}
+
+document.querySelector('.lista_cadastro input[type=submit]').addEventListener('click', adicionarItem);
+
+document.querySelector('button[name=limpar]').addEventListener('click', () => {
     items = [];
+    atualizarLista();
+});
 
-    document.querySelector('.lista_produtos').innerHTML = "";
-    document.querySelector('.soma h1').innerHTML = "Total: R$0"
-})
+atualizarLista();
+
 
